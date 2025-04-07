d3cee2381ccc36157d01a399579433ba5441ca21
vickash <vickashmahabir@gmail.com>
2025-04-06 23:45:08 -0400

    Formatting fix

--- examples/i2c_hw_search.rb
+++ examples/i2c_hw_search.rb
@@ -3,7 +3,7 @@ board = Denko::Board.new
 bus = Denko::I2C::Bus.new(board: board, index: I2C_DEV)
 bus.search
 
-print "I2C addresses on the bus:"
+puts "I2C addresses on the bus:"
 bus.found_devices.each do |device|
   puts "0x#{device.to_s(16).upcase}"
 end
