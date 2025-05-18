b005716547107070bddc671eb182365b024d59c8
vickash <vickashmahabir@gmail.com>
2025-05-17 21:44:08 -0400

    DS3231: change example so it reads RTC and sets system time

--- examples/ds3231.rb
+++ examples/ds3231.rb
@@ -14,14 +14,18 @@ unless (i2c.found_devices.include? ADDRESS)
   return
 end
 
-puts "Using DS3231 RTC at address Ox#{ADDRESS.to_s(16).upcase}"; puts
+puts "Using DS3231 RTC at address Ox#{ADDRESS.to_s(16).upcase}";
 rtc = Denko::RTC::DS3231.new(bus: i2c, address: ADDRESS)
 
-t = Time.now
-puts "Setting RTC to match system time: #{t}"
-rtc.time = t
+# Get time from RTC and make it into a string Linux can parse.
+t = rtc.time
+time_string = t.year.to_s.rjust(4, "0") + "-" +
+              t.month.to_s.rjust(2, "0") + "-" +
+              t.day.to_s.rjust(2, "0") + " " +
+              t.hour.to_s.rjust(2, "0") + ":" +
+              t.min.to_s.rjust(2, "0") + ":" +
+              t.sec.to_s.rjust(2, "0")
 
-5.times do
-  puts "RTC time is: now #{rtc.time}"
-  sleep 5
-end
+# Set the sytem time.
+`date -s \"#{time_string}\"`
+puts "Set sytem time to match RTC: #{time_string}"
